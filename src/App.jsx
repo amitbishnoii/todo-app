b608f32f915cf69f4649c0313586c9fb1d3e282d
amitbishnoii <amitbishnoi29012@gmail.com>
2025-09-27 21:45:51 +0530

    update styling for todo display

--- src/App.jsx
+++ src/App.jsx
@@ -24,18 +24,18 @@ function App() {
 
         <h1 className='font-bold text-xl'>Your Tasks</h1>
 
-        <div className="add-todos flex items-center gap-2.5 text-white">
+        <div className="add-todos flex items-center gap-2.5 text-white mt-3">
 
-          <input type="text" value={todo} placeholder="Add new Task" className="bg-neutral-500 p-1.5 pl-1.5 rounded-xl" onChange={handleChange} />
+          <input type="text" value={todo} placeholder="Add new Task" className="bg-neutral-500 p-1.5 pl-1.5 rounded-xl w-[240px]" onChange={handleChange} />
 
           <button onClick={handleAdd} className="bg-blue-600 p-2 hover:bg-blue-800 rounded-xl cursor-pointer text-sm font-bold">Add Todo</button>
 
         </div>
 
         {todos.map(item => {
-          return <div className="flex todo-card my-2.5 gap-2.5 items-center">
+          return <div className="flex todo-card my-2.5 gap-2.5 items-center justify-between w-[30%] mt-6">
             <div className="text">{item.todo}</div>
-            <div className="buttons flex gap-1.5 items-center">
+            <div className="buttons flex gap-2.5 items-center">
               <button className="bg-green-700 p-1 w-16">Edit</button>
               <button className="bg-red-600 p-1 w-16">Delete</button>
             </div>
